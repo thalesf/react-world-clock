dd03f25865749d1e1434c744a84314b1fae52183
Thales <thales.fdasilva@gmail.com>
2019-05-05 16:33:28 -0300

    Router errors fixed and Layout added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from "./pages/home";
+import Layout from "./layout/layout";
+
 import Clock from "./pages/clock";
 import GlobalStyles from "./components/shared/GlobalStyles";
 
@@ -9,14 +10,15 @@ const App = () => {
   return (
     <div className="App">
       <Router>
-        <GlobalStyles />
-        <Home />
-        <Switch>
-          <Route
-            path="/timezone/:timezone/:country?/:city?"
-            render={({ match }) => <Clock {...match} />}
-          />
-        </Switch>
+        <Layout>
+          <GlobalStyles />
+          <Switch>
+            <Route
+              path="/timezone/:timezone/:country?/:city?"
+              render={({ match }) => <Clock {...match} />}
+            />
+          </Switch>
+        </Layout>
       </Router>
     </div>
   );
